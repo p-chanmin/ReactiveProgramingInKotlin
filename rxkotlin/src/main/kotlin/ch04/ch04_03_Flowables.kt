ad5c1465e0a49b66b5e3b10f195053f0d5054d93
p-chanmin <pc_m111@naver.com>
2022-08-08 19:29:54 +0900

    Add Flowables

---
+++ rxkotlin/src/main/kotlin/ch04/ch04_03_Flowables.kt
@@ -0,0 +1,24 @@
+package ch04
+
+import io.reactivex.Flowable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+
+fun main(args: Array<String>) {
+    Flowable.range(1,1000)//(1)
+        .map { MyItem4(it) }//(2)
+        .observeOn(Schedulers.computation())
+        .subscribe({//(3)
+            print("Received $it;\t")
+            runBlocking { delay(50) }//(4)
+        },{it.printStackTrace()})
+    runBlocking { delay(70000) }//(5)
+}
+
+
+data class MyItem4 (val id:Int) {
+    init {
+        print("MyItem Created $id;\t")
+    }
+}
