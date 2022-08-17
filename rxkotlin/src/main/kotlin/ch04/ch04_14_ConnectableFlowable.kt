1a9910d60feda492a240311725014b4476148d42
p-chanmin <pc_m111@naver.com>
2022-08-17 20:18:09 +0900

    Add ch04_14_ConnectableFlowable

---
+++ rxkotlin/src/main/kotlin/ch04/ch04_14_ConnectableFlowable.kt
@@ -0,0 +1,19 @@
+package ch04
+
+import io.reactivex.rxkotlin.toFlowable
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+
+fun main(args: Array<String>) {
+    val connectableFlowable = listOf("String 1","String 2","String 3","String 4","String 5").toFlowable()
+        .publish()
+    connectableFlowable.
+    subscribe({
+        println("Subscription 1: $it")
+        runBlocking { delay(1000) }
+        println("Subscription 1 delay")
+    })
+    connectableFlowable
+        .subscribe({ println("Subscription 2 $it")})
+    connectableFlowable.connect()
+}
