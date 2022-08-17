e4b9f4b6e3815069f09f3432e6e4871970de063b
p-chanmin <pc_m111@naver.com>
2022-08-17 20:17:47 +0900

    Add ch04_12_BackpressureStrategyMissingLatest

---
+++ rxkotlin/src/main/kotlin/ch04/ch04_12_BackpressureStrategyMissingLatest.kt
@@ -0,0 +1,26 @@
+package ch04
+
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+
+fun main(args: Array<String>) {
+    val source = Observable.range(1, 1000)
+    source.toFlowable(BackpressureStrategy.MISSING)//(1)
+        .onBackpressureLatest()//(2)
+        .map { MyItem13(it) }
+        .observeOn(Schedulers.io())
+        .subscribe{
+            println("-> $it;\t")
+            runBlocking { delay(100) }
+        }
+    runBlocking { delay(600000) }
+}
+
+data class MyItem13 (val id:Int) {
+    init {
+        println("init $id;\t")
+    }
+}
